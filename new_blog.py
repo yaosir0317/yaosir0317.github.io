c5085ccdf42a589a0862418f5d2880388a7177e2
yaojiaqi <yaojiaqi@yyuehd.com>
2021-03-29 15:37:44 +0800

    update new_blog script

--- new_blog.py
+++ new_blog.py
@@ -1,29 +1,36 @@
 import os
+import sys
 from datetime import datetime
 
 
-path = os.path.dirname(__file__) + "/source/_posts"
-name = "Java基本数据类型"
-date = ""
-cat = "Java"
+path = "./source/_posts"
 
 
-file_name = os.path.join(path, "{0}.md".format(name))
-create_file = os.path.join(path, name)
-if not os.path.isdir(create_file):
-    os.makedirs(create_file)
+def gen_blog(name: str, date: str, cate: str):
+    file_name = os.path.join(path, "{0}.md".format(name))
+    create_file = os.path.join(path, name)
+    if not os.path.isdir(create_file):
+        os.makedirs(create_file)
 
-if not os.path.isfile(file_name):
-    f = open(file_name, "w")
-    content_list = (
-        "---",
-        "title: {0}".format(name),
-        "date: {0}".format(date or datetime.now().strftime("%Y-%m-%d %H:%M:%S")),
-        "tags: Java", "categories: {0}".format(cat),
-        "---",
-        "",
-        "<!--more-->"
-    )
-    for content in content_list:
-        f.write(content + "\n")
-    f.close()
+    if not os.path.isfile(file_name):
+        f = open(file_name, "w")
+        content_list = (
+            "---",
+            "title: {0}".format(name),
+            "date: {0}".format(date or datetime.now().strftime("%Y-%m-%d %H:%M:%S")),
+            "tags: {0}".format(cat), 
+            "categories: {0}".format(cat),
+            "---",
+            "",
+            "<!--more-->"
+        )
+        for content in content_list:
+            f.write(content + "\n")
+        f.close()
+
+
+if __name__ == '__main__':
+    name = sys.argv[1]
+    date = sys.argv[2]
+    cat = sys.argv[3]
+    gen_blog(name, date, cat)
